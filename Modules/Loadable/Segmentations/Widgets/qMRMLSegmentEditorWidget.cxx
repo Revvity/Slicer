540f3dae0e75df39e296f89a34e13d1e5149cbe5
James Butler <james.butler@perkinelmer.com>
2023-02-07 17:54:33 -0500

    BUG: Fix setting of Segment Editor parameters
    This reverts https://github.com/Slicer/Slicer/commit/a328293e72649c45d7f833c6a969b45861a6b267.
    
    The following type error was observed:
    
    [Qt] void __cdecl qMRMLSegmentEditorWidget::setSourceVolumeNode(class vtkMRMLNode *) failed: need to set segment editor and segmentation nodes first

--- Modules/Loadable/Segmentations/Widgets/qMRMLSegmentEditorWidget.cxx
+++ Modules/Loadable/Segmentations/Widgets/qMRMLSegmentEditorWidget.cxx
@@ -1179,7 +1179,7 @@ void qMRMLSegmentEditorWidget::updateEffectList()
 void qMRMLSegmentEditorWidget::updateWidgetFromMRML()
 {
   Q_D(qMRMLSegmentEditorWidget);
-  if (!this->mrmlScene() || this->mrmlScene()->IsClosing() || this->mrmlScene()->IsBatchProcessing())
+  if (!this->mrmlScene() || this->mrmlScene()->IsClosing())
     {
     return;
     }
@@ -1778,38 +1778,12 @@ void qMRMLSegmentEditorWidget::setMRMLScene(vtkMRMLScene* newScene)
 
   // observe close event so can re-add a parameters node if necessary
   this->qvtkConnect(this->mrmlScene(), vtkMRMLScene::EndCloseEvent, this, SLOT(onMRMLSceneEndCloseEvent()));
-  this->qvtkConnect(this->mrmlScene(), vtkMRMLScene::EndBatchProcessEvent, this, SLOT(onMRMLSceneEndBatchProcessEvent()));
 }
 
 //-----------------------------------------------------------------------------
 void qMRMLSegmentEditorWidget::onMRMLSceneEndCloseEvent()
 {
   this->initializeParameterSetNode();
-  this->updateWidgetFromMRML();
-}
-
-//-----------------------------------------------------------------------------
-void qMRMLSegmentEditorWidget::onMRMLSceneEndBatchProcessEvent()
-{
-  Q_D(qMRMLSegmentEditorWidget);
-  if (!this->mrmlScene())
-    {
-    return;
-    }
-
-  if (d->ParameterSetNode)
-    {
-    if (d->ParameterSetNode->GetSegmentationNode() != d->SegmentationNodeComboBox->currentNode())
-      {
-      this->setSegmentationNode(d->SegmentationNodeComboBox->currentNode());
-      }
-    if (d->ParameterSetNode->GetSourceVolumeNode() != d->SourceVolumeNodeComboBox->currentNode())
-      {
-      this->setSourceVolumeNode(d->SourceVolumeNodeComboBox->currentNode());
-      }
-    }
-
-  // force update (clear GUI if no node is selected anymore)
   this->updateWidgetFromMRML();
 }
 
@@ -2030,10 +2004,6 @@ QString qMRMLSegmentEditorWidget::sourceVolumeNodeID()const
 void qMRMLSegmentEditorWidget::onSegmentationNodeChanged(vtkMRMLNode* node)
 {
   Q_D(qMRMLSegmentEditorWidget);
-  if (!this->mrmlScene() || this->mrmlScene()->IsBatchProcessing())
-    {
-    return;
-    }
   this->setSegmentationNode(node);
 }
 
@@ -2097,10 +2067,6 @@ void qMRMLSegmentEditorWidget::setCurrentSegmentID(const QString segmentID)
 void qMRMLSegmentEditorWidget::onSourceVolumeNodeChanged(vtkMRMLNode* node)
 {
   Q_D(qMRMLSegmentEditorWidget);
-  if (!this->mrmlScene() || this->mrmlScene()->IsBatchProcessing())
-    {
-    return;
-    }
   this->setSourceVolumeNode(node);
 }
 
